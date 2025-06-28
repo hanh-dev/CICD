634b296cfd4bed81a2f665eaae04cf64e1a20ce0
Hanh_Dev Clean <hanh.ho26@student.passerellesnumeriques.org>
2025-06-28 10:50:52 +0700

    hello

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,15 +1,19 @@
-export async function generateMetadata({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  return { title: `Post: ${params.slug}` };
+// app/blog/[slug]/page.tsx
+
+type PageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+// generateMetadata
+export async function generateMetadata({ params }: PageProps) {
+  return {
+    title: `Post: ${params.slug}`,
+  };
 }
 
-export default function Page({
-  params,
-}: {
-  params: { slug: string };
-}) {
+// Component Page
+export default function Page({ params }: PageProps) {
   return <h1>Slug: {params.slug}</h1>;
 }
